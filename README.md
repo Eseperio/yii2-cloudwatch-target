0c9ca336833ce4270dd1f1c4e762d00c81acd608
E.Alamo <erosdelalamo@gmail.com>
2021-11-24 20:58:30 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,87 @@
+Yii2 Cloudwatch Logs Target
+===========================
+
+## Working in 2021
+
+This is a fork from Codemonauts but fixed with some pr of other users.
+
+A Yii2 log target for AWS Cloudwatch Logs.
+
+## Installation and Configuration
+
+Install the package through [composer](http://getcomposer.org):
+
+    composer require codemonauts/yii2-cloudwatch-logs
+
+And then add this to your application configuration:
+
+```php
+<?php
+return [
+    // ...
+    'components' => [
+        // ...
+        'log' => [
+            'targets' => [
+                [
+                    'class' => \eseperio\cloudwatch\Target::class,
+                    'region' => 'eu-west-1',
+                    'logGroup' => '/webserver/production/my-craft',
+                    'logStream' => 'instance-1', // omit for automatic instance ID
+                    'levels' => ['error'],
+                    'except' => ['yii\web\HttpException:404'],
+                    'logVars' => ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER'],
+                    'key' => 'your-key', // omit for instance role
+                    'secret' => 'your-secret', // omit for instance role
+                ],
+                // ...
+            ],
+        ],
+```
+
+## Configuration Options
+
+* (*string*) `$region` (required) The name of the AWS region e.g. eu-west-1
+* (*string*) `$logGroup` (required) The name of the log group.
+* (*string*) `$logStream` (optional) The name of the log stream. If omitted, it will try to determine the ID of the EC2
+  instance running on.
+* (*array*)  `$levels` (optional) Log level. Default by Yii2: ['error', 'warning', 'info', 'trace', 'profile']
+* (*array*)  `$logVars` (optional) Variables to log. Default by
+  Yii2: ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER']
+* (*string*) `$key` (optional) Your AWS access key.
+* (*string*) `$secret` (optional) Your AWS secret.
+
+## Rate limit
+
+The Cloudwatch Logs API requires a sequence token to be send with the messages to a log stream. This token is returned
+after a transfer of messages to the log stream to be used at the next transfer. For multi-process systems this is hard
+to handle.
+
+So AWS Cloudwatch Logs does not support multiple processes writing to the same log stream at the same time. Their
+official approach is to create a separate log stream for each process on each instance. But think of PHP-FPM with
+dynamic childs on several instances. This would end up in thousands of abandoned log streams in a log group after a
+while. Empty log streams are not deleted automatically and remain in the log groups. They do not cost anything, but make
+log groups very messy.
+
+Our approach is to create only one log stream per instance. To be able to send log streams from multiple processes at
+the same time, the current sequence token of the log stream must be queried before each transmission. But the AWS
+Cloudwatch Log API has a rate limit of 60 requests per second for this type of request.
+
+In order to avoid running into this rate limit, it is **highly advisable** to pass only those logs directly to
+Cloudwatch Logs that happen rarely and are important. For example Warnings and Errors.
+
+In large infrastructures with many simultaneous processes with a high log load, it is still better to use file logging
+and the Cloudwatch Log Agent, as this is only a single process.
+
+Maybe this will change in the future.
+
+## Cloudwatch Logs Insights
+
+If you want to parse the logs with Insights, then do something like this:
+
+```
+fields @timestamp, @message
+| sort @timestamp desc
+| limit 20
+| parse '[*][*][*][*][*] *' as ip, userId, sessionId, logLevel, category, message
+```
---
+++ src/Target.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace eseperio\cloudwatch;
+
+use yii\log\Target as BaseTarget;
+use yii\base\InvalidConfigException;
+use Aws\CloudWatchLogs\CloudWatchLogsClient;
+use yii\log\Logger;
+use yii\helpers\VarDumper;
+
+class Target extends BaseTarget
+{
+    /**
+     * @var string The name of the log group.
+     */
+    public $logGroup;
+
+    /**
+     * @var string The AWS region to use e.g. eu-west-1.
+     */
+    public $region;
+
+    /**
+     * @var string Your AWS access key.
+     */
+    public $key;
+
+    /**
+     * @var string The name of the log stream. When not set, we try to get the ID of your EC2 instance.
+     */
+    public $logStream;
+
+    /**
+     * @var string Your AWS secret.
+     */
+    public $secret;
+
+    /**
+     * @var CloudWatchLogsClient
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $sequenceToken;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if (empty($this->logGroup)) {
+            throw new InvalidConfigException("A log group must be set.");
+        }
+
+        if (empty($this->region)) {
+            throw new InvalidConfigException("The AWS region must be set.");
+        }
+
+        if (empty($this->logStream)) {
+            if (empty($this->key)) {
+                $instanceId = @file_get_contents("http://instance-data/latest/meta-data/instance-id");
+                if ($instanceId !== false) {
+                    $this->logStream = $instanceId;
+                } else {
+                    throw new InvalidConfigException("Cannot identify instance ID and no log stream name is set.");
+                }
+            } else {
+                throw new InvalidConfigException("No log stream name is set.");
+            }
+        }
+
+        $params = [
+            'region' => $this->region,
+            'version' => 'latest',
+        ];
+
+        if (!empty($this->key) && !empty($this->secret)) {
+            $params['credentials'] = [
+                'key' => $this->key,
+                'secret' => $this->secret,
+            ];
+        }
+
+        $this->client = new CloudWatchLogsClient($params);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function export()
+    {
+        $this->ensureLogGroupExists();
+
+        $this->refreshSequenceToken();
+
+        $data = [
+            'logEvents' => array_map([$this, 'formatMessage'], $this->messages),
+            'logGroupName' => $this->logGroup,
+            'logStreamName' => $this->logStream,
+        ];
+
+        if (!empty($this->sequenceToken)) {
+            $data['sequenceToken'] = $this->sequenceToken;
+        }
+        $logEvents = $data['logEvents'];
+
+        usort($logEvents, function (array $a, array $b) {
+            return $a['timestamp'] > $b['timestamp'] ? 1 : -1;
+        });
+        $data['logEvents'] = $logEvents;
+
+        $response = $this->client->putLogEvents($data);
+
+        $this->sequenceToken = $response->get('nextSequenceToken');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function formatMessage($message)
+    {
+        list($text, $level, $category, $timestamp) = $message;
+        $level = Logger::getLevelName($level);
+        if (!is_string($text)) {
+            // exceptions may not be serializable if in the call stack somewhere is a Closure
+            if ($text instanceof \Throwable || $text instanceof \Exception) {
+                $text = (string)$text;
+            } else {
+                $text = VarDumper::export($text);
+            }
+        }
+        $traces = [];
+        if (isset($message[4])) {
+            foreach ($message[4] as $trace) {
+                $traces[] = "in {$trace['file']}:{$trace['line']}";
+            }
+        }
+
+        $prefix = $this->getMessagePrefix($message);
+
+        return [
+            'timestamp' => $timestamp * 1000,
+            'message' => "{$prefix}[$level][$category] $text" . (empty($traces) ? '' : "\n    " . implode("\n    ", $traces))
+        ];
+    }
+
+    /**
+     * Get the sequence token for the selected log stream.
+     *
+     * @return void
+     */
+    private function refreshSequenceToken()
+    {
+        $existingStreams = $this->client->describeLogStreams([
+            'logGroupName' => $this->logGroup,
+            'logStreamNamePrefix' => $this->logStream,
+        ])->get('logStreams');
+
+        $exists = false;
+
+        foreach ($existingStreams as $stream) {
+            if ($stream['logStreamName'] === $this->logStream) {
+                $exists = true;
+                if (isset($stream['uploadSequenceToken'])) {
+                    $this->sequenceToken = $stream['uploadSequenceToken'];
+                }
+            }
+        }
+
+        if (!$exists) {
+            $this->client->createLogStream([
+                'logGroupName' => $this->logGroup,
+                'logStreamName' => $this->logStream,
+            ]);
+        }
+    }
+
+    /**
+     * Ensures that the selected log group exists or create it
+     *
+     * @return void
+     */
+    private function ensureLogGroupExists()
+    {
+        $existingGroups = $this->client->describeLogGroups([
+            'logGroupNamePrefix' => $this->logGroup,
+        ])->get('logGroups');
+
+        $exists = false;
+
+        foreach ($existingGroups as $group) {
+            if ($group['logGroupName'] === $this->logGroup) {
+                $exists = true;
+            }
+        }
+
+        if (!$exists) {
+            $this->client->createLogGroup([
+                'logGroupName' => $this->logGroup,
+            ]);
+        }
+    }
+}
